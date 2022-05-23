03ac5be4b37ec077f38b359d48b5bb6602bc2f37
Holden Halford <holdenhalford@Holdens-MBP.lan>
2022-05-22 23:09:59 -0700

    removed the update functionality from the getFormValues() function

--- Code.js
+++ Code.js
@@ -105,17 +105,10 @@ function processFinalForm(formObject) {
 /* GET FORM VALUES AS AN ARRAY */
 function getFormValues(formObject){
 /* ADD OR REMOVE VARIABLES ACCORDING TO YOUR FORM*/
-  if(formObject.RecId && checkID(formObject.RecId)){
-    var values = [[formObject.RecId.toString(),
+  var values = [[new Date().getTime().toString(),//https://webapps.stackexchange.com/a/51012/244121
                   formObject.d0120,
                   formObject.d0140,
                   formObject.d0145]];
-  }else{
-    var values = [[new Date().getTime().toString(),//https://webapps.stackexchange.com/a/51012/244121
-                  formObject.d0120,
-                  formObject.d0140,
-                  formObject.d0145]];
-  }
   return values;
 }
 
